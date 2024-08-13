30282e5d488e67ed9c8020cc705ce52d4c367d20
FernandoSegregio <segregio@gmail.com>
2024-08-13 00:23:36 -0300

    feat: update pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [react(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+    registerType: 'autoUpdate',
 
     pwaAssets: {
       disabled: false,
@@ -14,23 +13,57 @@ export default defineConfig({
     },
 
     manifest: {
+      id: '/',
       name: 'CodeXperience',
       short_name: 'CodeXperience',
       description: 'O melhor evento para programadores na Zona da Mata',
+      orientation: "portrait",
       theme_color: '#000000',
+      background_color: '#ffffff',
+      display: 'standalone',
+      "display_override": ["standalone", "minimal-ui"],
+      start_url: '/',
+      icons: [
+        {
+          src: '/icon-192x192.png',
+          sizes: '192x192',
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: '/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
     },
 
     workbox: {
       globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
       cleanupOutdatedCaches: true,
       clientsClaim: true,
+      skipWaiting: true,
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => {
+            return url.pathname.startsWith("/image");
+          },
+          handler: "CacheFirst",
+          options: {
+            cacheName: "runtime-cache",
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+      ],
     },
 
     devOptions: {
-      enabled: false,
+      enabled: true,
       navigateFallback: 'index.html',
-      suppressWarnings: true,
+      suppressWarnings: false,
       type: 'module',
     },
   })],
-})
+});
