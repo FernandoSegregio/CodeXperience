0660f4a0aad51a2987526a76922af88a7321e7a7
FernandoSegregio <segregio@gmail.com>
2024-08-19 15:24:15 -0300

    style: chage heigth countdonw

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       style={{ paddingTop: 'env(safe-area-inset-top)', paddingBottom: 'env(safe-area-inset-bottom)' }}
     >
       <Hero />
-      <div className="relative h-48 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 overflow-hidden">
+      <div className="relative h-64 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 overflow-hidden">
         <div className="absolute inset-0 bg-white bg-opacity-20"></div>
         <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 animate-pulse opacity-70"></div>
         <div className="relative z-10 flex items-center justify-center h-full text-white">
