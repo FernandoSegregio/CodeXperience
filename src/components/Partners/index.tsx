20ca754d91090b5ccd1b5523dd5604dbd939d913
FernandoSegregio <segregio@gmail.com>
2024-08-27 15:24:17 -0300

    fix: fix read pdf

--- src/components/Partners/index.tsx
+++ src/components/Partners/index.tsx
@@ -7,13 +7,15 @@ import techHub from '@assets/tech_hub.webp'
 export default function Partners() {
   const devops = "https://devopsdays.org/events/2024-juiz-de-fora/welcome/"
   const tech = "https://techhubjf.org/"
+  const rubeusUrl = "https://techhubjf.org/"
+  const uniUrl = "https://www.uniacademia.edu.br/"
 
   return (
     <div id='6' className="w-full bg-black py-24 flex flex-col justify-center items-center gap-6 sm:gap-12">
       <h3 className="text-white text-center text-2xl sm:text-5xl">Veja abaixo nossos parceiros</h3>
       <h3 className="text-white text-center px-2 text-lg max-w-72 sm:text-4xl sm:max-w-7xl">Quer se juntar com quem faz o CodeXperience acontecer?</h3>
       <a className="text-center rounded-xl text-black py-4 px-6 text-xl bg-[#0af40c] hover:bg-[#be73ed] hover:text-white transition duration-1000 ease-in-out sm:p-4 md:text-3xl"
-        href="/SejaCode2024.pdf" target="_blank" rel="noopener noreferrer"
+        href="SejaCode2024.pdf" target="_blank" rel="noopener noreferrer"
       >Patrocine o melhor evento da região</a>
 
       <div className="md:max-w-7xl max-w-72 w-full flex flex-col gap-8 md:gap-16">
@@ -22,8 +24,12 @@ export default function Partners() {
             PLATINA
           </p>
           <div className="w-2/3 flex flex-col justify-start py-2 sm:items-center sm:justify-center sm:flex-row sm:gap-8 sm:w-full sm:-ml-8">
-            <img src={uniAcademia} alt="logo UniAcademia patrocinador platina" width={150} className="-ms-5 sm:w-[250px] sm:ms-0" />
-            <img src={rubeus} alt="logo Runeus patrocinador platina" width={120} className="sm:w-[350px] sm:pb-5" />
+            <a href={uniUrl} className='cursor-pointer' target='_blank' rel="noopener noreferrer">
+              <img src={uniAcademia} alt="logo UniAcademia patrocinador platina" width={150} className="-ms-5 sm:w-[250px] sm:ms-0" />
+            </a>
+            <a href={rubeusUrl} className='cursor-pointer' target='_blank' rel="noopener noreferrer">
+              <img src={rubeus} alt="logo Runeus patrocinador platina" width={120} className="sm:w-[350px] sm:pb-5" />
+            </a>
           </div>
         </div>
         <div className="hidden flex-col sm:flex-row gap-8 md:gap-16 w-full">
