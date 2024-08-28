7cef9dbb21846d1d0c4b6d1b0f68ce0d2fa295e2
FernandoSegregio <segregio@gmail.com>
2024-08-27 22:41:18 -0300

    feat: update responsive for tablet size

--- src/components/Location/index.tsx
+++ src/components/Location/index.tsx
@@ -6,9 +6,9 @@ export default function Location() {
     <div className="bg-black flex flex-col items-center justify-center py-12 sm:py-40"
       id='5'
     >
-      <h3 className="text-white max-w-72 text-2xl text-center md:text-6xl pt-2 sm:max-w-7xl">O CodeXperience acontece na UniAcademia,</h3>
+      <h3 className="text-white max-w-72 text-2xl text-center pt-2 md:text-6xl md:max-w-xl lg:max-w-7xl">O CodeXperience acontece na UniAcademia,</h3>
       <h3 className="text-white max-w-72 text-xl p-2 pb-8  md:text-5xl md:pb-20 sm:max-w-7xl">no coração de Juiz de Fora!</h3>
-      <div className='flex flex-col md:flex-row gap-12 md:gap-48'>
+      <div className='flex flex-col gap-12 lg:flex-row lg:gap-48 sm:items-center'>
         <div className='md:w-1/2 flex flex-col gap-12 items-center'>
           <div className='bg-[#0af40c] px-4 w-64 py-28 rounded-3xl relative md:w-96 md:h-96'>
             <img src={location1} className='w-96 rounded-3xl absolute top-6 md:right-24 md:top-16' />
@@ -19,13 +19,13 @@ export default function Location() {
         </div>
 
         <div className='md:w-1/2 flex flex-col items-center'>
-          <div className='bg-[#be73ed] w-64 h-56 md:px-16 md:pt-20 rounded-3xl relative md:w-[400px] md:h-[480px]'>
+          <div className='bg-[#be73ed] w-64 h-56 rounded-3xl relative sm:px-8 sm:pt-10 sm:w-96 sm:h-[480px] lg:px-16 lg:pt-20 lg:w-[400px] lg:h-[480px]'>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3705.456636805489!2d-43.35566702395272!3d-21.762581180076804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x989b61e5bb4d59%3A0xab281c69be317217!2sCol%C3%A9gio%20Cristo%20Redentor%20-%20Academia%20de%20Com%C3%A9rcio!5e0!3m2!1spt-BR!2sbr!4v1724085130301!5m2!1spt-BR!2sbr"
               width="400"
               height="700"
               style={{ border: 0 }}
               loading="lazy"
-              className='w-56 h-56 absolute rounded-3xl -top-4 left-4 md:w-[400px] md:h-[530px] md:top-16 md:left-20'
+              className='w-56 h-56 absolute rounded-3xl -top-4 left-4 sm:w-80 sm:h-96 sm:top-12 sm:left-8 lg:w-[400px] lg:h-[530px] lg:top-16 lg:left-20'
               referrerPolicy="no-referrer-when-downgrade">
             </iframe>
           </div>
