df5d3df63f5fca819041cec8761e50669528d306
FernandoSegregio <segregio@gmail.com>
2024-08-30 10:13:55 -0300

    style: update format images

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,4 +1,5 @@
 import logoCode from '@assets/logo_code.webp';
+import logoCodeSmall from '@assets/codeSmall.webp';
 import codeSmall from '@assets/small2023.webp';
 import codeMedium from '@assets/medium2023.webp';
 import codeLarge from '@assets/large2023.webp';
@@ -8,10 +9,13 @@ export default function Hero() {
   return (
     <div id='1' className="w-full h-[39rem] relative flex flex-col items-center justify-center">
       <img
-        src={logoCode}
+        src={logoCodeSmall}
+        srcSet={`${logoCodeSmall} 400w, ${codeMedium} 600w`}
+        sizes="(max-width: 640px) 400px, 600px"
         className='absolute h-48 left-50 z-50 sm:h-[220px] md:top-52 md:h-[300px]'
         alt="Logo Code"
       />
+
       <Header />
       <div className='bg-black w-full h-full absolute opacity-60'></div>
       <img
@@ -19,7 +23,6 @@ export default function Hero() {
         srcSet={`${codeSmall} 600w, ${codeMedium} 1200w, ${codeLarge} 1800w`}
         sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 33vw"
         alt="Imagem codeXperience 2023"
-        // loading='lazy'
         className="object-cover w-full h-full"
       />
     </div>
