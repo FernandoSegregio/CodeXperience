2024872f7b06b12e48d23cc44d0be7cd63e4c82b
FernandoSegregio <segregio@gmail.com>
2024-10-31 21:27:13 -0300

    style: change color workshop

--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -12,7 +12,7 @@ const schedule: ScheduleItem[] = [
   { time: "10:00", room1: { theme: "Transição de Carreira", speaker: "Daniele Garcia de Castro Alves" }, room2: { theme: "Independência e Mocks: Como desenvolver e testar APIs sem depender de serviços reais.", speaker: "Camila Castro" }, room3: { theme: "-" } },
   { time: "10:40", room1: { theme: "Coffee Break", speaker: "" } },
   { time: "11:10", room1: { theme: "Gerenciamento de Defeitos e Métricas de Qualidade", speaker: "Emerson Pombo" }, room2: { theme: "Containers - O Microcosmos do Linux", speaker: "Paulo Cerqueira" }, room3: { theme: "Ganhe Dinheiro Programando para Si", speaker: "Igor Nascimento" } },
-  { time: "11:50", room1: { theme: "Como conseguir seu primeiro estágio, e ser desejado pelo mercado", speaker: "Tiago Gouvêa" }, room2: { theme: "DuckDB - O Patinho no Lago de Dados", speaker: "Danilo Oliveira Santos" }, room3: { theme: "A importância da inovação e tecnóloga em uma empresa do setor de serviços – Case EasyDr", speaker: "Nathan Vasconcellos" } },
+  { time: "11:50", room1: { theme: "Como conseguir seu primeiro estágio, e ser desejado pelo mercado", speaker: "Tiago Gouvêa" }, room2: { theme: "Workshop - DuckDB - O Patinho no Lago de Dados", speaker: "Danilo Oliveira Santos" }, room3: { theme: "A importância da inovação e tecnóloga em uma empresa do setor de serviços – Case EasyDr", speaker: "Nathan Vasconcellos" } },
   { time: "12:30", room1: { theme: "Almoço", speaker: "" }, },
   { time: "14:00", room1: { theme: "ignite talks", speaker: "" } },
   { time: "14:20", room1: { theme: "Saúde Física e Mental: Vamos Recarregar Nossas Baterias", speaker: "Rafael Rocha Ribeiro" }, room2: { theme: "Analisando falhas de Segurança nas Aplicaçoes Web", speaker: "Brendo Freitas" }, room3: { theme: "O papel do Consultor no Mercado de Tecnologia", speaker: "Higor Silva Souza (Higor Souza) & Maykon Vieira" } },
@@ -80,7 +80,7 @@ const ScheduleTable: React.FC = () => {
                   {/* Renderiza room2 e room3 apenas se não for a linha de almoço ou se room1 não for a única sala */}
                   {!isRoom1Only && (
                     <>
-                      <td className="px-4 py-2 border border-green-500">
+                      <td className={`px-4 py-2 border border-green-500 ${item.time === "11:50" ? "text-[#be73ed]" : ""}`}>
                         {item.room2 ? (
                           <>
                             <p className="font-bold">{item.room2.theme}</p>
@@ -137,7 +137,7 @@ const ScheduleTable: React.FC = () => {
             {item.room2 && item.room2?.theme !== '-' && (
               <div>
                 <p className="font-extrabold">Sala - Fortran</p>
-                <p className="">{item.room2.theme}</p>
+                <p className={`${item.time === "11:50" ? "text-[#be73ed]" : ""}`}>{item.room2.theme}</p>
                 {item.room2.speaker && <p className="text-gray-400">{item.room2.speaker}</p>}
               </div>
             )}
